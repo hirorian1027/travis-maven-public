e46d9e0823aa9077972249f63e9d4abf56ba2d57
ishikawa <ishikawahrzf@pm.nttdata-ccs.co.jp>
2019-12-25 13:44:02 +0900

    修正

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -10,7 +10,7 @@ public class SampleTest {
     @Test
     public void calcSuccess2() throws Exception {
     	Sample sample = new Sample();
-        assertEquals(-3, sample.calc(-1, -2), "計算が間違っています");
+        assertEquals(0, sample.calc(-1, -2), "計算が間違っています");
     }
     @Test
     public void calcSuccess3() throws Exception {
